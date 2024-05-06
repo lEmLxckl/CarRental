624d8ea50622d783e4cb227db3bbf05181cd406c
Emre <Emre2880@outlook.dk>
2024-05-06 16:29:31 +0200

    init

---
+++ src/main/java/org/example/carrental/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package org.example.carrental.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "home/index";
+    }
+}
