d48e6d8c4b01cdb76a4995e6d2f0f71701d34cac
DekaSkole <eser0004@stud.kea.dk>
2024-05-16 13:30:32 +0200

    first

--- src/main/java/org/example/carrental/controllers/BusinessDeveloperController.java
+++ src/main/java/org/example/carrental/controllers/BusinessDeveloperController.java
@@ -1,2 +1,52 @@
-package org.example.carrental.controllers;public class BusinessDeveloperController {
+package org.example.carrental.controllers;
+
+
+import org.example.carrental.Service.BusinessDeveloperService;
+import org.example.carrental.model.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BusinessDeveloperController {
+
+    @Autowired
+    private BusinessDeveloperService businessDeveloperService;
+
+    @GetMapping("/overview")
+    public String getOverview(Model model) {
+
+        // Henter vehicles fra service
+        List<Vehicle> vehicles = businessDeveloperService.getAllVehicles();
+        model.addAttribute("vehicles", vehicles);
+        return "overview";
+    }
+
+    @GetMapping("/overview/price")
+    public String sortByPrice(Model model) {
+
+        // Henter vehicles sorteret med pris fra service
+        List<Vehicle> vehicles = businessDeveloperService.getAllVehiclesSortedByPrice();
+        model.addAttribute("vehicles", vehicles);
+        return "overview";
+    }
+
+    @GetMapping("/overview/leased")
+    public String showLeasedVehicles(Model model) {
+        // Henter leaset vehicles fra service
+        List<Vehicle> vehicles = businessDeveloperService.getLeasedVehicles();
+        model.addAttribute("vehicles", vehicles);
+        return "overview";
+    }
+
+    @GetMapping("/overview/model")
+    public String sortByModel(Model model) {
+        // Henter vehicles sorteret efter model fra service
+        List<Vehicle> vehicles = businessDeveloperService.getAllVehiclesSortedByModel();
+        model.addAttribute("vehicles", vehicles);
+        return "overview";
+    }
 }
