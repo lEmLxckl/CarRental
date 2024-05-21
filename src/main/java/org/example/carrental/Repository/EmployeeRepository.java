cf4c86be93d30a35464789a2089388e4700c9e1f
arfi <arma0001@stud.kea.dk>
2024-05-21 20:48:56 +0200

    "commit"

--- src/main/java/org/example/carrental/Repository/EmployeeRepository.java
+++ src/main/java/org/example/carrental/Repository/EmployeeRepository.java
@@ -4,7 +4,6 @@ import org.example.carrental.model.Employee;
 import org.example.carrental.model.Usertype;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
