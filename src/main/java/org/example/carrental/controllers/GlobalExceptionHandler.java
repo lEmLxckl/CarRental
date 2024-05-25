b3ebbd47980fbc447f9f06c12663c8a4e6518846
Emre <Emre2880@outlook.dk>
2024-05-25 18:39:12 +0200

    30

---
+++ src/main/java/org/example/carrental/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package org.example.carrental.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+    @Controller
+    public class GlobalExceptionHandler {
+        @ExceptionHandler(Exception.class)
+        public String handleException(Exception e, Model model) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "home/error";  // Ensure this is the correct path to your error view
+        }
+    }
+
